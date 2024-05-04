056615faeea0d24b2cda4514a5e689952448ac38
mdiazguerra <167119214+mdiazguerra@users.noreply.github.com>
2024-05-04 20:54:37 +0200

    Update project_programming_part_2.py

--- project_programming_part_2.py
+++ project_programming_part_2.py
@@ -15,13 +15,6 @@ from PIL import Image
 import streamlit as st
 
 
-# Print logo
-image = Image.open('Logo-KDT-JU.webp') # Load the image from disk
-st.image(image)
-
-# Print title
-st.title('Partner search tool')
-
 # This function is expected to connect to the database and access the countries dataframe.
 def get_countries_df():
     conn = sqlite3.connect('ecsel_database.db')
@@ -84,6 +77,12 @@ def get_yearly(selected_country):
 # This would be the main programme
 
 def main():
+    # Print logo
+    image = Image.open('Logo-KDT-JU.webp') # Load the image from disk
+    st.image(image)
+
+    # Print title
+    st.title('Partner search tool')
 
     # Using this function you can access the table of countries in order to retrieve the necessary acronyms from the dictionary.
     countries_df = get_countries_df()
