4ca81fe26fecfed0d194eba5b636d964275be8cf
mdiazguerra <167119214+mdiazguerra@users.noreply.github.com>
2024-05-10 18:57:21 +0200

    Update project_programming_part_2.py

--- project_programming_part_2.py
+++ project_programming_part_2.py
@@ -117,8 +117,8 @@ def main():
     st.subheader("Yearly EC contribution (€) in " + selected_country)
     plott = get_yearly(selected_country_acronym)
     #plot = pd.DataFrame(plot)
-    #st.bar_chart(plot)
-    st.bar_chart(plott.set_index('year'))
+    st.bar_chart(plott)
+    #st.bar_chart(plott.set_index('year'))
     
     # Generate and display participants dataframe of the selected country
     participants_df = get_participants_df(selected_country_acronym)
